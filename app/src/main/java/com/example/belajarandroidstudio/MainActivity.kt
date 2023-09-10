04744bd22306486a53d61349567e6cfc0f2676e3
MalfinK <muhammadalfin1235678@gmail.com>
2023-09-10 23:35:00 +0700

    Bahasan view id

--- app/src/main/java/com/example/belajarandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/belajarandroidstudio/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.belajarandroidstudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hello_world)
+
+        // mendapatkan id dari file hello_world
+        // R.id.nameEditText
+
+        // mau ambil semua view nya dari id yang sudah di tambahkan di hello_world
+        val nameEditText: EditText = findViewById(R.id.nameEditText)
+        val sayHelloButton: Button = findViewById(R.id.sayHelloButton)
+        val sayHelloTextView: TextView = findViewById(R.id.sayHelloTextView)
+
+        sayHelloTextView.text = "Hi"
     }
 }
